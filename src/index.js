7f00283d17c3f8919269108988a1eb366a26be55
thefalked <giulianocrivelli@hotmail.com>
2021-06-29 23:11:11 -0400

    adding routes: get, post, put, patch and delete

--- src/index.js
+++ src/index.js
@@ -2,8 +2,35 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World ignite!"});
+app.use(express.json());
+
+app.get("/courses", (request, response) => {
+  const query = request.query;
+  console.log(query);
+
+  return response.json(["Course 1", "Course 2", "Course 3"]);
+});
+
+app.post("/courses", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
+  return response.json(["Course 1", "Course 2", "Course 3", "Course 4"]);
+});
+
+app.put("/courses/:id", (request, response) => {
+  const { id } = request.params;
+  console.log(id);
+
+  return response.json(["Course 6", "Course 2", "Course 3", "Course 4"]);
+});
+
+app.patch("/courses/:id", (request, response) => {
+  return response.json(["Course 6", "Course 7", "Course 3", "Course 4"]);
+});
+
+app.delete("/courses/:id", (request, response) => {
+  return response.json(["Course 6", "Course 7", "Course 4"]);
 });
 
 app.listen(3333);
